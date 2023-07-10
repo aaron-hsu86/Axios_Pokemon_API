69798143cbe3e9e26ebdddb9cfb7360ce2704aab
Aaron <aaron.hsu86+github@gmail.com>
2023-07-10 02:01:00 -0700

    first axios commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const [pokeList, setPokemon] = useState([]);
+  const [pokeList2, setPokemon2] = useState([])
+
+  const getPokeList = () => {
+    fetch('https://pokeapi.co/api/v2/pokemon?limit=807&offset=0')
+    .then(response => {
+      return response.json();
+    }).then(response =>{
+      // console.log(response)
+      setPokemon(response.results);
+    }).catch(err=>{
+      console.log(err);
+    });
+  }
+
+  const axiosPokeList = () => {
+    axios.get('https://pokeapi.co/api/v2/pokemon?limit=807&offset=0').then(response=>{
+      console.log(response);
+      return response;
+    }).then( response => {
+      setPokemon2(response.data.results);
+    }).catch(err=>{
+      console.log(err)
+    });
+  }
+
+  // console.log(pokeList2)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='lists'>
+        <div>
+        <button onClick={axiosPokeList}>List 807 Pokemon vis Axios</button>
+          <p>Axios Pokemon list:</p>
+          <ul>
+          {pokeList2.length > 0 && pokeList2.map((pokemon, index) => {
+            return (<li key={index+1}>{index+1} - {pokemon.name}</li>)
+          })}
+          </ul>
+        </div>
+        <div>
+        {pokeList.length==0?<button onClick={getPokeList}>List 807 Pokemon!</button>:<></>}
+          <p>Fetch Pokemon list:</p>
+          <ul>
+          {pokeList.length > 0 && pokeList.map((pokemon, index) => {
+            return (<li key={index+1}>{index+1} - {pokemon.name}</li>)
+          })}
+          </ul>
+        </div>
+      </div>
+    </>
   );
 }
 
